9aa0e5c7e17b953e5e70204009042121927f029d
Yuno_g <hogheong@naver.com>
2022-08-10 15:26:47 +0900

    about 수정

--- components/about.vue
+++ components/about.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>Hello about!</h1>
+  <v-flex xs12>
+    <p class="text-lg-center display-1">About</p>
+
+    <p class="text-lg-center title">
+      <span class="cyan--text darken-3">Vue Enterprise Release Management</span>
+    </p>
+
+    <p class="text-xs-left subheader">
+      This demo app is not a real application.<br>
+      it just a kind of concept yet<br>
+      frontend made of Vue(+vuetify)<br>
+      backend made of express<br>
+      The main purpose of this application is managing release version
+    </p>
+  </v-flex>
 </template>
--- components/userList.vue
+++ components/userList.vue
@@ -1,5 +1,90 @@
 <template>
-  <div>hi! users!</div>
+  <v-container fluid>
+    <v-flex xs12>
+      <v-card>
+        <v-card-title>
+          <span class="title"
+            >Customers {{ pagination ? "(" + pagination.totalItems + ")" : "" }}
+          </span>
+          <v-spacer></v-spacer>
+          <table-header-buttons
+            :add="add"
+            :reloadData="reloadData"
+            :updateSearchPanel="updateSearchPanel"
+          ></table-header-buttons>
+        </v-card-title>
+        <Table
+          v-if="loading === false"
+          :headers="headers"
+          :items="items"
+          :pagination="pagination"
+          @edit="edit"
+          @remove="remove"
+        ></Table>
+      </v-card>
+    </v-flex>
+    <search-panel
+      :rightDrawer="rightDrawer"
+      @cancelSearch="cancelSearch"
+      @searchData="searchCustomers"
+    >
+      <v-layout row>
+        <v-flex xs11 offset-xs1>
+          <v-text-field
+            name="input-1-3"
+            label="Frist Name"
+            light
+          ></v-text-field>
+        </v-flex>
+      </v-layout>
+      <v-layout row>
+        <v-flex xs11 offset-xs1>
+          <v-text-field
+            name="input-1-3"
+            label="Last Name"
+            light
+          ></v-text-field>
+        </v-flex>
+      </v-layout>
+      <v-layout row>
+        <v-flex xs11 offset-xs1>
+          <v-text-field
+            name="input-1-3"
+            label="Mobile"
+            light
+          ></v-text-field>
+        </v-flex>
+      </v-layout>
+            <v-layout row>
+        <v-flex xs11 offset-xs1>
+          <v-text-field
+            name="input-1-3"
+            label="Email"
+            light
+          ></v-text-field>
+        </v-flex>
+      </v-layout>
+     
+    </search-panel>
+
+    <confirm-dialog
+      :dialog="dialog"
+      :dialogTitle="dialogTitle"
+      :dialogText="dialogText"
+      @onConfirm="onConfirm"
+      @onCancel="onCancel"
+    />
+    <v-snackbar
+      v-if="loading === false"
+      :right="true"
+      :timeout="2000"
+      :color="mode"
+      v-model="snackbar"
+    >
+      {{ notice }}
+      <v-btn dark text @click.native="closeSnackbar">Close</v-btn>
+    </v-snackbar>
+  </v-container>
 </template>
 
 <script>
